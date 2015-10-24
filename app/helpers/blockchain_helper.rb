137b17d0e424a68ddf4ed4852118c388dba73eb1
kkennis <kevinvkennis@gmail.com>
2015-10-24 15:48:05 -0700

    Add blockchain helper

--- app/helpers/blockchain_helper.rb
+++ app/helpers/blockchain_helper.rb
@@ -1,2 +1,12 @@
-def send(user, amount)
+module BlockChainHelper
+  require 'blockchain'
+
+  # Add shit from the gem here
+
+  def send(user, amount)
+  end
+
+  def receive(user, amount)
+  end
+
 end
